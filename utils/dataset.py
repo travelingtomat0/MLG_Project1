e2db66c45876e38e35e06d5faec5a194458304c5
Philip Toma <tomap@student.ethz.ch>
2022-04-01 20:23:15 +0200

    Commit dataset progress.

--- utils/dataset.py
+++ utils/dataset.py
@@ -2,13 +2,14 @@ import numpy as np
 from torch.utils.data import IterableDataset, Dataset, random_split, ConcatDataset
 import os
 import pandas as pd
+import pyBigWig
 
 """
 NOTE: The dataset is a work in progress. For now, we only load X1 cell line data!!!
 """
 class InputDataset(Dataset):
 
-    def __init__(self, data_directory='./', window_size=200000, cell_line='X1', modality_names=[('', '')]):
+    def __init__(self, data_directory='./', window_size=200000, cell_line='X1', modality_names=[]):
         super(InputDataset, self).__init__()
         self.data_path = data_directory
         self.cell_line = cell_line
@@ -26,32 +27,37 @@ class InputDataset(Dataset):
         # set length of dataset.
         self.length = self.train_info.shape[0]
 
-        # Generate data per gene
-        for gene in self.train_info['gene_name']:
-            pass
-        self.data = []
-        for name in modality_names:
-            self.data.append(self.load_modality(name))
+        # Data is fetched when indexing the dataset.
+        # Implemented in __get_item__().
+        pass
 
     def load_modality(self, name):
         modality_data = pd.read_csv(os.path.join(self.data_path, name, 'X1.bed'), sep='\t', header=None)
         modality_data = pd.read_csv(os.path.join(self.data_path, name, 'X1.bigwig'), sep='\t', header=None)
         return modality_data
 
-    def get_bed_vector(self, modality, chr, TSS_start, TSS_end):
-        bed_file = pd.read_csv(os.path.join(self.data_path, modality, f'{self.cell_line}.bed'))
+    def get_bed_vector(self, modality, chr, start, end, TSS_start):
+        bed_file = pd.read_csv(os.path.join(self.data_path, f'{modality}-bed/{self.cell_line}.bed'))
         # TODO: HOW ORIENTATION?
-        resulting_peaks = np.zeros((2*self.window_size + (TSS_end - TSS_start)))
+        resulting_peaks = np.zeros((2*self.window_size))
         peaks = bed_file.loc[(bed_file[0] == chr) &
-                             (bed_file[1] > TSS_start - self.window_size) &
-                             (bed_file[2] < TSS_end + self.window_size)]
+                             (bed_file[1] >= start) &
+                             (bed_file[2] <= end)]
         # one-hot encoding of the peaks...
-        for peak in peaks:
-            for i in range(0):
-                pass
+        for i in range(len(peaks)):
+            for j in range(peaks[i:i+1][1][0], peaks[i:i+1][2][0]):
+                transformed_index = j - (TSS_start - self.window_size)
+                resulting_peaks[transformed_index] = 1.0
         bed_file = None
+        return resulting_peaks
 
-    pd.read_csv('./X1.bed', sep='\t', header=None)
+    def get_bigwig_vector(self, modality, chr, start, end):
+        try:
+            bw = pyBigWig.open(os.path.join(self.data_path, f'{modality}-bigwig/{self.cell_line}.bigwig'))
+        except:
+            bw = pyBigWig.open(os.path.join(self.data_path, f'{modality}-bigwig/{self.cell_line}.bw'))
+        resulting_vector = bw.values(chr, start, end)
+        return np.array(resulting_vector)
 
     def __len__(self):
         return self.length
@@ -59,6 +65,20 @@ class InputDataset(Dataset):
     def __getitem__(self, index):
         # index+1 because 0 returns the header
         info = self.train_info[index:index+1]
-        TSS = (self.train_info['TSS_start'][index], self.train_info['TSS_end'][index])
-        gene_region = (self.train_info['gene_start'][index], self.train_info['gene_end'][index])
-        chr = self.train_info['chr'][index]
+
+        # Get the window
+        lowest_location = info['TSS_start'][0] - self.window_size
+        highest_location = info['TSS_end'][0] + self.window_size
+
+        # Which cromosome is the gene located on?
+        chr = info['chr'][0]
+
+        # TODO: HOW TO USE STRAND +/- information?
+
+        # Get binary input vector eg. methylation, acetylation etc. from .bed files.
+        for m in self.modality_names:
+            bed_vec = self.get_bed_vector(m, chr, lowest_location, highest_location, info['TSS_start'][0])
+            bw_vec = self.get_bigwig_vector(self, m, chr, lowest_location, highest_location)
+
+        # TODO: CONCATENATE VECTORS
+        # TODO: Use sequence information?
